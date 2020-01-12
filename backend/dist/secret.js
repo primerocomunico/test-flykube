9f17b94130e7832a6be2e2f30d253828237ea8f0
unknown <sergio@primerocomunico.es>
2020-01-12 03:09:33 +0100

    delete access file

--- backend/dist/secret.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbAccess = {
-    "URI": "mongodb+srv://Tsarkon:Gonzaloges_1804@mymongo-8ndql.mongodb.net/test?retryWrites=true&w=majority"
-};
